ca58f32b3f6e37f78a20222d03f3700d8c696e37
Akshay-9189 <akshaykumbhar957@gmail.com>
2023-04-19 12:35:32 +0530

    security updates

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,26 +1,27 @@
 import axios from "axios";
 import { userService } from "../constants";
+import AuthHeader from './AuthHeader'
 
 class UserService {
 
     getAllUsers() {
-        return axios.get(userService.USER + '/')
+        return axios.get(userService.USER + '/', { headers: AuthHeader() })
     }
 
     deleteUser(userId) {
-        return axios.delete(userService.USER + `/delete/${userId}`)
+        return axios.delete(userService.USER + `/delete/${userId}`, { headers: AuthHeader() })
     }
 
     getUser(userId) {
-        return axios.get(userService.USER + `/get/${userId}`)
+        return axios.get(userService.USER + `/get/${userId}`, { headers: AuthHeader() })
     }
 
     updateUser(userId, userInfo) {
-        return axios.put(userService.USER + `/update/${userId}`, userInfo)
+        return axios.put(userService.USER + `/update/${userId}`, userInfo, { headers: AuthHeader() })
     }
 
     changeUserRole(userId) {
-        return axios.patch(userService.USER + `/change-role/${userId}`)
+        return axios.patch(userService.USER + `/change-role/${userId}`, { headers: AuthHeader() })
     }
 }
 
